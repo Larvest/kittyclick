82e777aa5f36790d311bb1f62e609676c6a79e0a
Larvest <58668760+Larvest@users.noreply.github.com>
2023-06-20 01:20:04 +0300

    система сохранений

--- cliker.js
+++ cliker.js
@@ -15,6 +15,7 @@ var clicker = {
         }
     }
 };
+var delay = 0;
 function food_clicked(ass){
      if(clicker.upgrades[ass].cost <= clicker.food){
     clicker.food-= clicker.upgrades[ass].cost;
@@ -26,15 +27,29 @@ function food_clicked(ass){
 function update_upgrades(){
     document.querySelector("#upgrades").innerHTML="";
     for(i in clicker.upgrades){
-        document.querySelector("#upgrades").innerHTML+= `<br><button onclick="food_clicked('${i}')">${clicker.upgrades[i].name}</button> у вас <b>${clicker.upgrades[i].amount}</b>. Цена: ${clicker.upgrades[i].cost}` //втф че это блять 
+        document.querySelector("#upgrades").innerHTML+= `<br><button onclick="food_clicked('${i}')">${clicker.upgrades[i].name}</button> у вас ${clicker.upgrades[i].amount}. Цена: ${clicker.upgrades[i].cost}` //втф че это блять 
     }
 }
 function updatecount(){ // не трогать желательно
     update_upgrades();
+    if(Cookies.get("clicker") != null && Cookies.get("clicker") != "undefined"){
+        var clicker1 = JSON.parse(Cookies.get("clicker"));
+        for(i in clicker.upgrades){
+            if(clicker1.upgrades[i] == null){
+                clicker1.upgrades[i] == clicker.update_upgrades[i]; //типо когда игра будет обновлятьсчя прогресс не сбросится я хз
+            }
+        }
+        clicker = clicker1
+    }
     setInterval(() => {
     for(i in clicker.upgrades){
-        clicker.food+=clicker.upgrades[i].amount*clicker.upgrades[i].fps/20
+        clicker.food+=clicker.upgrades[i].amount*clicker.upgrades[i].fps/20;
     }
     document.querySelector("#food").innerHTML = "У вас "+String(clicker.food).split(".")[0]+" корма"
+    delay++;
+    if(delay >= 40){
+        Cookies.set("clicker",JSON.stringify(clicker), {expires: 100000}) // переводит кликер в стринг что бы потом опять перевести в переменную, 100000(дней) - это как долго будут храниться данные
+        delay = 0;
+    }
     }, 50);
 }
