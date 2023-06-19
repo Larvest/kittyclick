a493e5678d3246f7aab22b4e2b1752b43b95f5d2
Larvest <58668760+Larvest@users.noreply.github.com>
2023-06-20 00:41:23 +0300

    хуйййййййййййййййй

--- cliker.js
+++ cliker.js
@@ -6,13 +6,27 @@ var clicker = {
             cost:100,
             fps:1,
             name:"Кот Японец"
+        },
+        gentleman_cat:{
+            amount:0,
+            cost:500,
+            fps:5,
+            name:"Кот Джентельмен"
         }
     }
 };
 function food_clicked(ass){ // я хз как это работает, но каждый клик дает 1 еду
-    clicker[ass]++;
+    clicker.upgrades[ass].amount++;
+    update_upgrades();
+}
+function update_upgrades(){
+    document.querySelector("#upgrades").innerHTML="";
+    for(i in clicker.upgrades){
+        document.querySelector("#upgrades").innerHTML+= `<br><button onclick="food_clicked('${i}')">${clicker.upgrades[i].name}<button> у вас ${clicker.upgrades[i].amount}` //втф че это блять
+    }
 }
 function updatecount(){ // не трогать желательно
+    update_upgrades();
     setInterval(() => {
     for(i in clicker.upgrades){
         clicker.food+=clicker.upgrades[i].amount*clicker.upgrades[i].fps/20
