8e41e482848aa44d2ee8b7c1e60ece00f84a7dac
SirCat07 <46454817+SirCat07@users.noreply.github.com>
2023-06-20 11:37:27 +0300

    :exploding_head:

--- cliker.js
+++ cliker.js
@@ -1,55 +1,66 @@
 var clicker = {
-    food:0,
+    food: 10000,
+    foodPerSecond: 0,
     upgrades: { // это апгрейды. каждый апгрейд сопроваждается параметрами как количество, цена и еда в секунду
-        japanese_cat:{
-            amount:0,
-            cost:100,
-            fps:1,
-            name:"Кот Японец"
+        japanese_cat: {
+            amount: 0,
+            cost: 100,
+            fps: 1,
+            name: "Кот Японец"
         },
-        gentleman_cat:{
-            amount:0,
-            cost:500,
-            fps:5,
-            name:"Кот Джентельмен"
+        gentleman_cat: {
+            amount: 0,
+            cost: 500,
+            fps: 5,
+            name: "Кот Джентельмен"
+        },
+        boykisser_cat: {
+            amount: 0,
+            cost: 621,
+            fps: 1,
+            name: "Бойкиссер"
         }
     }
 };
 var delay = 0;
-function food_clicked(ass){
-     if(clicker.upgrades[ass].cost <= clicker.food){
-    clicker.food-= clicker.upgrades[ass].cost;
-    clicker.upgrades[ass].amount++;
-    clicker.upgrades[ass].cost += Math.round(clicker.upgrades[ass].cost*0.15); // ну это как бы ебать ваще я не ебу че делает, но это короче формула увеличивающая цену с каждой покупкой
-    update_upgrades();
+function food_clicked(ass) {
+    if (clicker.upgrades[ass].cost <= clicker.food) {
+        clicker.food -= clicker.upgrades[ass].cost;
+        clicker.upgrades[ass].amount++;
+        clicker.upgrades[ass].cost += Math.round(clicker.upgrades[ass].cost * 0.15); // ну это как бы ебать ваще я не ебу че делает, но это короче формула увеличивающая цену с каждой покупкой
+        update_upgrades();
     }
 }
-function update_upgrades(){
-    document.querySelector("#upgrades").innerHTML="";
-    for(i in clicker.upgrades){
-        document.querySelector("#upgrades").innerHTML+= `<br><button onclick="food_clicked('${i}')">${clicker.upgrades[i].name}</button> у вас <b>${clicker.upgrades[i].amount}</b>. Цена: ${clicker.upgrades[i].cost}` //втф че это блять 
+function update_upgrades() {
+    document.querySelector("#upgrades").innerHTML = "";
+    for (i in clicker.upgrades) {
+        document.querySelector("#upgrades").innerHTML += `<br><button onclick="food_clicked('${i}')">${clicker.upgrades[i].name}</button> у вас <b>${clicker.upgrades[i].amount}</b>. Цена: ${clicker.upgrades[i].cost}` //втф че это блять 
     }
 }
-function updatecount(){ // не трогать желательно
+function updatecount() { // не трогать желательно
     update_upgrades();
-    if(Cookies.get("clicker") != null && Cookies.get("clicker") != "undefined"){
+    if (Cookies.get("clicker") != null && Cookies.get("clicker") != "undefined") {
         var clicker1 = JSON.parse(Cookies.get("clicker"));
-        for(i in clicker.upgrades){
-            if(clicker1.upgrades[i] == null){
+        for (i in clicker.upgrades) {
+            if (clicker1.upgrades[i] == null) {
                 clicker1.upgrades[i] == clicker.update_upgrades[i]; //типо когда игра будет обновлятьсчя прогресс не сбросится я хз
             }
         }
         clicker = clicker1
     }
     setInterval(() => {
-    for(i in clicker.upgrades){
-        clicker.food+=clicker.upgrades[i].amount*clicker.upgrades[i].fps/20;
-    }
-    document.querySelector("#food").innerHTML = "У вас "+String(clicker.food).split(".")[0]+" корма"
-    delay++;
-    if(delay >= 40){
-        Cookies.set("clicker",JSON.stringify(clicker), {expires: 100000}) // переводит кликер в стринг что бы потом опять перевести в переменную, 100000(дней) - это как долго будут храниться данные
-        delay = 0;
-    }
+        for (i in clicker.upgrades) {
+            clicker.food += clicker.upgrades[i].amount * clicker.upgrades[i].fps / 20;
+            clicker.foodPerSecond += clicker.upgrades[i].amount * clicker.upgrades[i].fps;
+        }
+        document.querySelector("#food").innerHTML = "У вас " + String(clicker.food).split(".")[0] + " корма"
+        document.querySelector("#foodPerSecond").innerHTML = "У вас " + String(clicker.foodPerSecond).split(".")[0] + " корма в секунду"
+        clicker.foodPerSecond = 0;
+
+        delay++;
+        if (delay >= 40) {
+            Cookies.set("clicker", JSON.stringify(clicker), { expires: 100000 }) // переводит кликер в стринг что бы потом опять перевести в переменную, 100000(дней) - это как долго будут храниться данные
+            delay = 0; // :exploding_head:
+        }
     }, 50);
 }
